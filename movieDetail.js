9e02b8ec15a13603da6775bf5b744aaf4f99dc99
PhilippeInCode <felipechacon.montero@gmail.com>
2024-11-09 13:24:00 +0100

    Add more funcionality and visibility when you click on film

--- movieDetail.js
+++ movieDetail.js
@@ -1,21 +1,22 @@
 // Obtener el ID de la película desde la URL
 const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id');
-console.log("Film ID:", movieId); 
+console.log("Film ID:", movieId);
 
 const requestURL = './json/films.json';
 
 async function fetchMovieData() {
     const response = await fetch(requestURL);
     const data = await response.json();
-    console.log("Film Data:", data.films); 
-    return data.films.find(movie => movie.id === movieId); 
+    console.log("Film Data:", data.films);
+    return data.films.find(movie => movie.id === movieId);
 }
 
 // Renderizar la información de la película en la página
 fetchMovieData().then(movie => {
     if (movie) {
         document.getElementById('movieTitle').textContent = movie.title;
+        document.getElementById('movieTitleCard').textContent = movie.title;  
         document.getElementById('moviePoster').src = movie.poster;
         document.getElementById('movieDirector').textContent = `Director: ${movie.director}`;
         document.getElementById('movieYear').textContent = `Year: ${movie.year}`;
